b044828db7ea085561cb42fd67dea67de722936b
JustJucier <ju2237210046@gmail.com>
2025-09-09 17:55:13 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by jujin on 25-9-9.
+//
+#include "sprint.hpp"
+
+int main() {
+}
---
+++ sprint.hpp
@@ -0,0 +1,9 @@
+//
+// Created by jujin on 25-9-9.
+//
+
+#ifndef SPRINT_HPP
+
+#define SPRINT_HPP
+
+#endif //SPRINT_HPP
