4c7b78420421a5f17c777815591e492cadc6dfe2
JustJucier <ju2237210046@gmail.com>
2025-09-09 18:17:28 +0800

    support pair<T, U>

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 // Created by jujin on 25-9-9.
 //
 #include "sprint.hpp"
-
+#include <print>
 int main() {
-
+    {
+        std::pair p{1, 3.4};
+        std::println("[std]: pair: {}", p);
+    }
 }
--- sprint.hpp
+++ sprint.hpp
@@ -4,6 +4,21 @@
 
 #ifndef SPRINT_HPP
 #include <format>
+/////////////////////// STD TYPE SPECIALIZATION ////////////////////////////
+template<typename T1, typename T2>
+struct std::formatter<std::pair<T1, T2>> {
+    constexpr auto parse(std::format_parse_context& ctx) {
+        return ctx.begin();
+    }
+    auto format(const std::pair<T1, T2>& pair, std::format_context& ctx) const {
+        auto out = ctx.out();
+        out = std::format_to(out, "({}, {})", pair.first, pair.second);
+        return out;
+    }
+};
+
+
+/////////////////////// STD TYPE SPECIALIZATION ////////////////////////////
 void print() {
 
 }
